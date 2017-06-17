00ef53d7ebc0384682d13f6aaf149da3a9e29183
April Ross <aprilross@Aprils-MacBook-Pro.local>
2017-06-17 17:12:07 -0400

    revisions2

--- main.js
+++ main.js
@@ -14,13 +14,40 @@ let submitbutton = document.getElementsByClassName("submitbutton");
 //   event.preventDefault();
 // };
 
+function createNode(element){
+  return document.createElement(element);
+}
+
+function append(parent, el){
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('tracks');
+
 document.addEventListener('click', function(e){
   if(e.target.className === "submitbutton")
-  // var artist = artist.innerHTML;
+
   fetch("https://api.soundcloud.com/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&" + artist.innerHTML)
   .then(function(response){
   response.json().then(function(data){
     console.log(data);
+
+    // let artistInfo = data;
+
+// // Get the track image and name of track
+//     let li = createNode('li');
+//     let img = createNode('img');
+//     let name = createNode('span');
+//
+//     img.src = data.artwork_url;
+//     span.innerHTML = "";
+//
+// // Append the info to the unordered list with classname tracks
+//     append(li, img);
+//     append(li, span);
+//     append(ul, li);
+
+
   })
 })
 });
